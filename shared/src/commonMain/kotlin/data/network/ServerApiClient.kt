058ca7abd58745279f74a7d1bd4cd8e1afd1770f
Denis Skarlat <bboyzlodey@gmail.com>
2021-10-27 23:22:37 +0300

    Add common server methods

--- shared/src/commonMain/kotlin/data/network/ServerApiClient.kt
+++ shared/src/commonMain/kotlin/data/network/ServerApiClient.kt
@@ -11,6 +11,7 @@ import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import utils.Const
 
 class ServerApiClient : ServerApi {
 
@@ -23,66 +24,66 @@ class ServerApiClient : ServerApi {
             logger = Logger.SIMPLE
             level = LogLevel.BODY
         }
-        engine { proxy = ProxyBuilder.http("http://localhost:8080") }
+        engine { proxy = ProxyBuilder.http(Const.TEST_SERVER_HOST) }
     }
 
     override suspend fun getStepRecords(): List<StepRecord> {
-        return client.get("/stepRecords")
+        return client.get(Const.GET_STEP_RECORDS_POINT)
     }
 
     override suspend fun getChallenges(): List<Challenge> {
-        return client.get("/challenges")
+        return client.get(Const.GET_CHALLENGES_POINT)
     }
 
     override suspend fun getProfile(): Profile {
         return client.get {
-            this.method = HttpMethod("/profile")
+            this.method = HttpMethod(Const.GET_PROFILE_POINT)
         }
     }
 
     override suspend fun getAntropometricInfo(): AntropometricInfo {
         return client.get {
-            this.method = HttpMethod("/antropometricInfo")
+            this.method = HttpMethod(Const.GET_STEP_RECORD_POINT)
         }
     }
 
     override suspend fun sendStepRecord(body: StepRecord): Any? {
         return client.put {
             this.body = body
-            this.method = HttpMethod("/sendStepRecord")
+            this.method = HttpMethod(Const.PUSH_STEP_RECORD_POINT)
         }
     }
 
     override suspend fun sendStepRecords(body: List<StepRecord>): Any? {
         return client.put {
             this.body = body
-            this.method = HttpMethod("/sendStepRecords")
+            this.method = HttpMethod(Const.PUSH_STEP_RECORDS_POINT)
         }
     }
 
     override suspend fun setAntropometricInfo(body: AntropometricInfo): Any? {
         return client.put {
             this.body = body
-            this.method = HttpMethod("/setAntropometricInfo")
+            this.method = HttpMethod(Const.PUSH_ANTROPOMETRIC_INFO_POINT)
         }
     }
 
     override suspend fun updateProfile(body: Profile): Any? {
         return client.put {
             this.body = body
-            this.method = HttpMethod("/updateProfile")
+            this.method = HttpMethod(Const.PUSH_PROFILE_POINT)
         }
     }
 
     override suspend fun logout(): Any? {
         return client.get {
-            this.method = HttpMethod("/logout")
+            this.method = HttpMethod(Const.LOGOUT_POINT)
         }
     }
 
     override suspend fun signInWithGoogle(): Any? {
         return client.get {
-            this.method = HttpMethod("/signInWithGoogle")
+            this.method = HttpMethod(Const.SIGN_IN_WITH_GOOGLE_POINT)
         }
     }
 }
---
+++ shared/src/commonMain/kotlin/utils/Const.kt
@@ -0,0 +1,20 @@
+package utils
+
+object Const {
+    const val TEST_SERVER_HOST = "http://localhost:8080"
+    const val PROD_SERVER_HOST = "TO DO"
+    const val GET_STEP_RECORDS_POINT = "/stepRecords"
+    const val GET_STEP_RECORD_POINT = "/stepRecord"
+    const val GET_CHALLENGES_POINT = "/challenges"
+    const val GET_PROFILE_POINT = "/profile"
+    const val GET_ANTROPOMETRIC_INFO_POINT = "/antropometricInfo"
+
+    const val PUSH_ANTROPOMETRIC_INFO_POINT = "/setAntropometricInfo"
+    const val PUSH_STEP_RECORD_POINT = "/sendStepRecord"
+    const val PUSH_STEP_RECORDS_POINT = "/sendStepRecords"
+    const val PUSH_PROFILE_POINT = "/updateProfile"
+
+    const val LOGOUT_POINT = "/logout"
+    const val SIGN_IN_WITH_GOOGLE_POINT = "/signInWithGoogle"
+
+}
