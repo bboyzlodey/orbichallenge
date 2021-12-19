4e0f08ba07acf85931da5e4c9d020115b7347ab7
Denis Skarlat <bboyzlodey@gmail.com>
2021-12-19 03:26:01 +0300

    Change user db password and server host

--- server/src/main/kotlin/com/skarlat_dev/domain/storage/db/MariaDB.kt
+++ server/src/main/kotlin/com/skarlat_dev/domain/storage/db/MariaDB.kt
@@ -15,7 +15,7 @@ class MariaDB : IDatabase {
     fun connect() {
         database = Database.connect(
             url = "jdbc:mysql://localhost:3306/test", driver = "com.mysql.cj.jdbc.Driver",
-            user = "denis", password = "denis90_1I",
+            user = "denis", password = "denis90_11",
             setupConnection = { connection ->
                 println("Connection to db is ${connection.isValid(3)}")
             }
